d149d7eb43abfbcaf05f403f196576f4142c30c2
Nigraum <yan.corintima@hotmail.com>
2022-10-02 20:47:42 -0300

    Finishing create map

--- scripts/index.js
+++ scripts/index.js
@@ -7,11 +7,15 @@ function onSound(index) {
 fetch('../sounds.json').then(response => response.json()).then(data => {
   sounds = data
 
+  const elementSounds = document.querySelector('.sounds')
+
   sounds.map((sound, index) => {
     const li = document.createElement('li')
     const button = document.createElement('button')
 
     button.setAttribute('onClick', `onSound(${index})`)
     
+    li.appendChild(button)
+    elementSounds.appendChild(li)
   })
 })

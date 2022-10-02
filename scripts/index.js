b6ba7d14d4ab0cfb0122f28409df3cad86ee6f68
Nigraum <yan.corintima@hotmail.com>
2022-10-02 20:43:13 -0300

    Create map for buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+let sounds = []
+
+fetch('../sounds.json').then(response => response.json()).then(data => {
+  sounds = data
+
+  sounds.map(sound => {
+    const li = document.createElement('li')
+    const button = document.createElement('button')
+
+    button.setAttribute('onClick', '')
+    
+  })
+})

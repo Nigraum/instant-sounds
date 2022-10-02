88a438f6b27807fc361401e97fc80ab786e6eeb6
Nigraum <yan.corintima@hotmail.com>
2022-10-02 20:46:01 -0300

    Create onsoud function and testing

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,17 @@
 let sounds = []
 
+function onSound(index) {
+  console.log('tocar o som', index)
+}
+
 fetch('../sounds.json').then(response => response.json()).then(data => {
   sounds = data
 
-  sounds.map(sound => {
+  sounds.map((sound, index) => {
     const li = document.createElement('li')
     const button = document.createElement('button')
 
-    button.setAttribute('onClick', '')
+    button.setAttribute('onClick', `onSound(${index})`)
     
   })
 })

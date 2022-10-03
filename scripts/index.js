52655a3ffb6e67afce885579cf8d352c1a06935b
Nigraum <yan.corintima@hotmail.com>
2022-10-02 21:15:44 -0300

    Finishing create map

--- scripts/index.js
+++ scripts/index.js
@@ -11,11 +11,20 @@ fetch('../sounds.json').then(response => response.json()).then(data => {
 
   sounds.map((sound, index) => {
     const li = document.createElement('li')
-    const button = document.createElement('button')
 
+    const button = document.createElement('button')
     button.setAttribute('onClick', `onSound(${index})`)
-    
+
+    const span = document.createElement('span')
+    span.innerHTML = sound.hotkey
+    button.appendChild(span)
+
+    const label = document.createElement('label')
+    label.innerHTML = sound.label
+    button.appendChild(label)
+
     li.appendChild(button)
+
     elementSounds.appendChild(li)
   })
 })
